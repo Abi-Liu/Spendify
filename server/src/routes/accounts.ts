b82768c146b20c2ac43a0684d5e7cfd8c671e7e0
Abi Liu <abiliu018@gmail.com>
2023-11-23 18:36:15 -0800

    routes for accounts

--- server/src/routes/accounts.ts
+++ server/src/routes/accounts.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-// fetches all transactions for a single account.
-
-// how would i implement redis cache for this and items/transactions route? Because the rdis cache only holds the last years worth of transactions, should i have a general route to hit the cache, and have another route if the user wants transaction details outside of the redis cache? I.E transactions for the past 2 years.
+router.get("/items/:itemId", () => {});
+router.get("/user/:userId", () => {});
+router.delete("/items/:itemId", () => {});
 
 export default router;
