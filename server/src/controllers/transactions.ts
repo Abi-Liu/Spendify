42c4a1aeec31f2657acb7e06e9ca8eaab0cb63d2
Abi Liu <abiliu018@gmail.com>
2023-12-05 18:48:55 -0800

    added routes for paginated transaction fetching

--- server/src/controllers/transactions.ts
+++ server/src/controllers/transactions.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
-import { getUserTransactionsFromDates } from "../database/transactions";
+import {
+  getPaginatedTransactions,
+  getUserTransactionsFromDates,
+} from "../database/transactions";
 import { sanitizeTransactions } from "../utils/sanitize";
 export default {
   getTransactionsByUser: async (req: Request, res: Response) => {
@@ -20,6 +23,24 @@ export default {
       if (transactions) {
         res.status(200).json(sanitizeTransactions(transactions));
       }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error fetching transactions" });
+    }
+  },
+
+  getTransactionsPagination: async (req: Request, res: Response) => {
+    try {
+      const { column, columnValue, offset, limit } = req.query;
+      // gets the next page of transactions
+      const transactions = await getPaginatedTransactions(
+        column as string,
+        columnValue as string,
+        Number(offset),
+        Number(limit)
+      );
+
+      res.status(200).json(sanitizeTransactions(transactions));
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Error fetching transactions" });
--- server/src/routes/transactions.ts
+++ server/src/routes/transactions.ts
@@ -4,5 +4,6 @@ import transactionsController from "../controllers/transactions";
 const router = express.Router();
 
 router.get("/user/:id", transactionsController.getTransactionsByUser);
+router.get("/pagination", transactionsController.getTransactionsPagination);
 
 export default router;
