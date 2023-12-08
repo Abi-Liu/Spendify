9682fded85d76420b706346316b4803979441005
Abi Liu <abiliu018@gmail.com>
2023-12-08 10:27:28 -0800

    loading error states for institutions context

--- client/src/contexts/InstitutionsContext.tsx
+++ client/src/contexts/InstitutionsContext.tsx
@@ -9,21 +9,30 @@ import React, {
 import { Institution } from "plaid";
 import api from "../utils/axios";
 
-interface InstitutionState {
+interface InstitutionsShape {
   byInstitutionId: { [id: string]: Institution };
   byItemId: { [id: number]: Institution };
 }
+interface InstitutionState {
+  institutions: InstitutionsShape;
+  loading: boolean;
+  error: string | null;
+}
 
 const initialState: InstitutionState = {
-  byInstitutionId: {},
-  byItemId: {},
+  institutions: { byInstitutionId: {}, byItemId: {} },
+  loading: false,
+  error: null,
 };
 
-type InstitutionActions = {
-  type: "SUCCESSFUL_GET";
-  payload: Institution;
-  itemId: number;
-};
+type InstitutionActions =
+  | {
+      type: "SUCCESSFUL_GET";
+      payload: Institution;
+      itemId: number;
+    }
+  | { type: "REQUEST_INSTITUTIONS" }
+  | { type: "ERROR"; payload: string };
 
 const reducer = (state: InstitutionState, action: InstitutionActions) => {
   switch (action.type) {
@@ -33,12 +42,25 @@ const reducer = (state: InstitutionState, action: InstitutionActions) => {
       }
       return {
         ...state,
-        byInstitutionId: {
-          ...state.byInstitutionId,
-          [action.payload.institution_id]: action.payload,
+        institutions: {
+          byInstitutionId: {
+            ...state.institutions.byInstitutionId,
+            [action.payload.institution_id]: action.payload,
+          },
+          byItemId: {
+            ...state.institutions.byItemId,
+            [action.itemId]: action.payload,
+          },
         },
-        byItemId: { ...state.byItemId, [action.itemId]: action.payload },
+        loading: false,
+        error: null,
       };
+    }
+    case "REQUEST_INSTITUTIONS": {
+      return { ...state, loading: true, error: null };
+    }
+    case "ERROR": {
+      return { ...state, loading: false, error: action.payload };
     }
     default:
       console.warn("Unkown action");
@@ -47,14 +69,20 @@ const reducer = (state: InstitutionState, action: InstitutionActions) => {
 };
 
 interface InstitutionsContextShape {
-  institutions: InstitutionState;
+  institutionsState: InstitutionState;
+  institutions: InstitutionsShape;
+  loading: boolean;
+  error: string | null;
   dispatch: Dispatch<InstitutionActions>;
   getInstitutionById: (id: string, itemId: number) => void;
   formatLogo: (logo: string | null | undefined) => string | null | undefined;
 }
 
 const InstitutionsContext = createContext<InstitutionsContextShape>({
-  institutions: initialState,
+  institutionsState: initialState,
+  institutions: { byInstitutionId: {}, byItemId: {} },
+  loading: false,
+  error: null,
   dispatch: () => {},
   getInstitutionById: async () => {},
   formatLogo,
@@ -63,16 +91,30 @@ const InstitutionsContext = createContext<InstitutionsContextShape>({
 export const InstitutionsProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [institutions, dispatch] = useReducer(reducer, initialState);
+  const [institutionsState, dispatch] = useReducer(reducer, initialState);
 
   const getInstitutionById = useCallback(async (id: string, itemId: number) => {
-    const { data } = await api.get(`/institutions/${id}`);
-    dispatch({ type: "SUCCESSFUL_GET", payload: data.institution, itemId });
+    dispatch({ type: "REQUEST_INSTITUTIONS" });
+    try {
+      const { data } = await api.get(`/institutions/${id}`);
+      dispatch({ type: "SUCCESSFUL_GET", payload: data.institution, itemId });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: "ERROR", payload: "Failed to get institution data" });
+    }
   }, []);
 
   return (
     <InstitutionsContext.Provider
-      value={{ institutions, dispatch, getInstitutionById, formatLogo }}
+      value={{
+        institutionsState,
+        institutions: institutionsState.institutions,
+        error: institutionsState.error,
+        loading: institutionsState.loading,
+        dispatch,
+        getInstitutionById,
+        formatLogo,
+      }}
     >
       {children}
     </InstitutionsContext.Provider>
