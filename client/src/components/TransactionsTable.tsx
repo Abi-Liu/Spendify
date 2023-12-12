8f102039f2cca5f49b785b20a8c2a7ae28e5ad49
Abi Liu <abiliu018@gmail.com>
2023-12-12 15:44:24 -0800

    render table

--- client/src/components/TransactionsTable.tsx
+++ client/src/components/TransactionsTable.tsx
@@ -4,6 +4,7 @@ import { Table } from "@mantine/core";
 import api from "../utils/axios";
 import useUserContext from "../contexts/UserContext";
 import Loading from "./Loading";
+import formatCurrency from "../utils/formatDollar";
 
 const TransactionsTable = () => {
   const [transactions, setTransactions] = useState<Transactions[]>();
@@ -38,15 +39,30 @@ const TransactionsTable = () => {
     return <Loading />;
   }
 
-  // column definitions
-  const columns = [
-    { accessorKey: "date", header: "Date" },
-    { accessorKey: "name", header: "Name" },
-    { accessorKey: "personal_finance_category", header: "Category" },
-    { accessorKey: "payment_channel", header: "Payment Channel" },
-    { accessorKey: "amount", header: "Amount" },
-  ];
-  return <div>TransactionsTable</div>;
+  const rows = transactions.map((transaction) => (
+    <Table.Tr key={transaction.id}>
+      <Table.Td>{transaction.date}</Table.Td>
+      <Table.Td>{transaction.name}</Table.Td>
+      <Table.Td>{transaction.personal_finance_category}</Table.Td>
+      <Table.Td>{transaction.payment_channel}</Table.Td>
+      <Table.Td>{formatCurrency(Number(transaction.amount))}</Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Date</Table.Th>
+          <Table.Th>Name</Table.Th>
+          <Table.Th>Category</Table.Th>
+          <Table.Th>Payment Channel</Table.Th>
+          <Table.Th>Amount</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+  );
 };
 
 export default TransactionsTable;
