c0c435c6996d48f553b6602b4ad760ade6603e35
Abi Liu <abiliu018@gmail.com>
2023-12-22 00:03:49 -0800

    budget creating modal

--- client/src/pages/BudgetPage.tsx
+++ client/src/pages/BudgetPage.tsx
@@ -1,13 +1,67 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import useBudgetsContext from "../contexts/BudgetsContext";
 import useItemsContext from "../contexts/ItemsContext";
 import NoAccounts from "../components/NoAccounts";
 import { Button, Container, Text, Modal, Input } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import useUserContext from "../contexts/UserContext";
+import { notifications } from "@mantine/notifications";
+
+const CustomForm = () => {
+  const [amount, setAmount] = useState<number | "">("");
+
+  const { createBudget } = useBudgetsContext();
+  const { user } = useUserContext();
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+
+    if (/^\d*$/.test(value) || value === "") {
+      setAmount(value === "" ? "" : parseInt(value)); // Convert valid input to a number
+    }
+  }
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    console.log("submitting");
+    e.preventDefault();
+    if (user && typeof amount === "number") {
+      createBudget(user.id, amount);
+      // show success message
+      notifications.show({
+        message: `Budget successfully created`,
+        color: "green",
+      });
+    } else {
+      // user will always be defined so if we get into this block
+      // that means the user has tried to submit an empty input
+      notifications.show({
+        title: `Amount can not be empty`,
+        message: "Please enter a valid amount",
+        color: "red",
+      });
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input.Wrapper label="Amount">
+        <Input
+          type="text"
+          inputMode="numeric"
+          value={amount}
+          onChange={handleChange}
+          placeholder="e.g 100"
+        />
+      </Input.Wrapper>
+      <Button type="submit" my="1rem">
+        Create
+      </Button>
+    </form>
+  );
+};
 
 const BudgetPage = () => {
   const [opened, { open, close }] = useDisclosure(false);
-  const [amount, setAmount] = useState(0);
   const { budgets } = useBudgetsContext();
   const { itemsArray } = useItemsContext();
 
@@ -24,10 +78,7 @@ const BudgetPage = () => {
           Create a monthly budget to keep your expenses in check!
         </Text>
         <Modal opened={opened} onClose={close} title="Monthly Budget">
-          <form>
-            <Input placeholder="e.g 100" />
-            <Button my="1rem">Create</Button>
-          </form>
+          <CustomForm />
         </Modal>
         <Button onClick={open}>Create Budget</Button>
       </Container>
