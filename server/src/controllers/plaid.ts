1b741cc803ce5d22528f36150b677d8a72311626
Abi Liu <abiliu018@gmail.com>
2023-12-29 15:25:38 -0800

    updated ngrok tunnel

--- server/src/controllers/plaid.ts
+++ server/src/controllers/plaid.ts
@@ -12,7 +12,7 @@ const { CLIENT_URL } = process.env;
 // only used for development. allows the local server to receive plaid webhooks
 // must be changed to match each new ngrok url
 const WEBHOOK_URL =
-  "https://7b85-2600-1700-a410-90b0-c4e2-f9ad-b078-bccb.ngrok-free.app/webhook/";
+  "https://d2e0-2600-1700-a410-90b0-2950-7c81-a16e-4669.ngrok-free.app/webhook/";
 
 export default {
   // start the link flow by sending client a public token.
