0cc6ae1b22afdbb8a5b7b7ce06d9b797f98568f4
Abi Liu <abiliu018@gmail.com>
2023-12-28 17:32:09 -0800

    refactored to use hash, this will avoid duplicate entries for the same dates

--- server/src/controllers/networth.ts
+++ server/src/controllers/networth.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from "express";
 import redis from "../config/redis";
 
-// limit window to 90 days
-const MAX_WINDOW_SIZE = 90;
-
 export default {
   // use a sliding window approach to keep the past 90 days of net worth data in redis.
   addNetworthData: async (req: Request, res: Response) => {
     const { userId, networth, date } = req.body;
     const key = `networth:${userId}`;
-    const value = { date, networth };
     try {
-      await redis.lpush(key, JSON.stringify(value));
-
-      // if list length is greater than 90, trim off the excess
-      // this will remove the oldest networth value if the length exceeds 90
-      await redis.ltrim(key, 0, MAX_WINDOW_SIZE - 1);
-      // retrieve the data to send back to user
-      const data = await redis.lrange(key, 0, -1);
+      // set the entry into the redis hash. the date will be used as the key
+      await redis.hset(key, date, networth);
+      // if the length is greater than 90, we have to slide the window closed until we are at 90 or below
+      const length = await redis.hlen(key);
+      if (length > 90) {
+        const keys = await redis.hkeys(key);
+        // because the oldest date will always be at the first key in the hash, we can just delete the first key
+        await redis.hdel(key, keys[0]);
+      }
+      const data = redis.hgetall(key);
       res.status(200).json(data);
     } catch (error) {
       console.error(error);
@@ -29,7 +28,7 @@ export default {
     const { userId } = req.params;
     const key = `networth:${userId}`;
     try {
-      const data = await redis.lrange(key, 0, -1);
+      const data = await redis.hgetall(key);
       res.status(200).json(data);
     } catch (error) {
       console.error(error);
