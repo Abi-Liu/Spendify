79d1d23a0c592e1102a4d3a413582f547366fa26
Abi Liu <abiliu018@gmail.com>
2024-01-12 18:52:09 -0800

    added db name

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,7 +1,8 @@
 import { Pool } from "pg";
 
 // create the connection to database
-const { DB_PORT, POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST } = process.env;
+const { DB_PORT, POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST, DB_NAME } =
+  process.env;
 
 if (!DB_PORT || !POSTGRES_USER || !POSTGRES_PASSWORD || !DB_HOST) {
   throw new Error("DB variables are undefined");
@@ -12,6 +13,7 @@ const connection = new Pool({
   port: parseInt(DB_PORT),
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
+  database: DB_NAME,
   max: 10,
   idleTimeoutMillis: 30000, // waits 30 seconds before closing idle connections
   connectionTimeoutMillis: 2000, // throws error if unable to connect to db after 2 seconds
