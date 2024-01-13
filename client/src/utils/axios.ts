32c7f85469e4acb96d049cb1bd56aa31fc9c5259
Abi Liu <abiliu018@gmail.com>
2024-01-12 16:49:36 -0800

    testing

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -6,6 +6,8 @@ console.log(VITE_ENV, VITE_SERVER_URL);
 
 const apiUrl = VITE_ENV === "dev" ? "http://localhost:8000" : VITE_SERVER_URL;
 
+console.log(apiUrl);
+
 const api = axios.create({
   baseURL: apiUrl,
   withCredentials: true,
