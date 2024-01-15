1732f5b3a7dddf92f0a7bd721c792a35e9dfed06
Abi Liu <abiliu018@gmail.com>
2024-01-15 14:14:13 -0800

    deleted console logs and added dist to gitignore

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const { VITE_SERVER_URL, VITE_ENV } = import.meta.env;
 
-console.log(VITE_ENV, VITE_SERVER_URL);
-
 const apiUrl = VITE_ENV === "dev" ? "http://localhost:8000" : VITE_SERVER_URL;
-
-console.log(apiUrl);
 
 const api = axios.create({
   baseURL: apiUrl,
