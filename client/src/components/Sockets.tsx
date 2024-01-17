e5940fc0f136efe22ec3ebe28bae97f7d2cb9bcd
Abi Liu <abiliu018@gmail.com>
2024-01-17 15:12:04 -0800

    fixed socket server url

--- client/src/components/Sockets.tsx
+++ client/src/components/Sockets.tsx
@@ -18,7 +18,10 @@ const Sockets = () => {
   const { generateItemLinkToken } = useLinkContext();
 
   useEffect(() => {
-    const socket = io("http://localhost:8000");
+    const { VITE_SERVER_URL, VITE_ENV } = import.meta.env;
+    const apiUrl =
+      VITE_ENV === "dev" ? "http://localhost:8000" : VITE_SERVER_URL;
+    const socket = io(apiUrl);
 
     // Send userId on socket connection to differentiate connections
     socket.on("connect", () => {
